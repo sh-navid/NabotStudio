c7ab21ce92af47804bbb40691cf77278032489b8
sh-navid <shamshirsaz.navid@live.com>
2025-07-12 06:40:36 +0330

    [Enhancement] Update the toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function Toolbar({ runCode }) {
+function Toolbar({ runCode, newTab }) {
   return (
     <div className="toolbar">
+      <button onClick={runCode}>Desktop</button>
+      <button onClick={runCode}>Tablet</button>
+      <button onClick={runCode}>Mobile</button>
+      <button onClick={runCode}>Responsive</button>
+      <span> | </span>
       <button onClick={runCode}>Run</button>
+      <button onClick={newTab}>Screen</button>
     </div>
   );
 }
