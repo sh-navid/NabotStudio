405195c1828f25a2b126ee28b8e85bf6978717b0
sh-navid <shamshirsaz.navid@live.com>
2025-07-13 21:35:00 +0330

    [Enhancement] Setup prismjs

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,12 +1,23 @@
-import React from 'react';
+/* */
+import React, { useEffect, useRef } from 'react';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css'; // Import the tomorrow theme
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 
 function Editor({ code, handleCodeChange }) {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    Prism.highlightElement(editorRef.current);
+  }, [code]);
+
   return (
-    <textarea
-      className="editor"
-      value={code}
-      onChange={handleCodeChange}
-    />
+    <pre className="line-numbers">
+      <code className="language-javascript" ref={editorRef}>
+        {code}
+      </code>
+    </pre>
   );
 }
 
